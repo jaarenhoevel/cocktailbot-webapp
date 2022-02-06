0bc70aa3314e106e24b6f6a7eb60ab6bb447ceb7
János Arenhövel <jaarenhoevel@gmail.com>
2022-02-06 22:01:01 +0100

    Add navbar links

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,11 +1,68 @@
-<script></script>
+<script>
+    import { link } from "svelte-routing";
+    // core components
+    let navbarOpen = false;
+    function setNavbarOpen() {
+        navbarOpen = !navbarOpen;
+    }
+</script>
 
-<nav class="top-0 fixed z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-white shadow">
-    <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
-        <div class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <a class="text-blueGray-700 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase" href="/">
+<nav
+    class="top-0 fixed z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-white shadow"
+>
+    <div
+        class="container px-4 mx-auto flex flex-wrap items-center justify-between"
+    >
+        <div
+            class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"
+        >
+            <a
+                use:link
+                class="text-blueGray-700 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+                href="/"
+            >
                 CocktailBot
             </a>
+            <button
+                class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                type="button"
+                on:click={setNavbarOpen}
+            >
+                <i class="fas fa-bars" />
+            </button>
+        </div>
+        <div
+            class="lg:flex flex-grow items-center {navbarOpen
+                ? 'block'
+                : 'hidden'}"
+            id="example-navbar-warning"
+        >
+            <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
+                <li class="flex items-center">
+                    <a
+                        class="hover:text-blueGray-500 text-blueGray-700 px-3 py-2 flex items-center text-xs uppercase font-bold"
+                        href="/"
+                    >
+                        <i
+                            class="text-blueGray-400 far fa-file-alt text-lg leading-lg mr-2"
+                        />
+                        Docs
+                    </a>
+                </li>
+
+                <li class="flex items-center">
+                    <a
+                        class="hover:text-blueGray-500 text-blueGray-700 px-3 py-2 flex items-center text-xs uppercase font-bold"
+                        href="https://github.com/jaarenhoevel/cocktailbot-core"
+                        target="_blank"
+                    >
+                        <i
+                            class="text-blueGray-400 fab fa-github text-lg leading-lg mr-2"
+                        />
+                        GitHub
+                    </a>
+                </li>
+            </ul>
         </div>
     </div>
 </nav>
