fecf21ad40bebd546b6f9afeef1a24a2170f8482
János Arenhövel <jaarenhoevel@gmail.com>
2022-02-15 20:16:41 +0100

    Use bot storage for status info

--- src/layouts/Control.svelte
+++ src/layouts/Control.svelte
@@ -11,37 +11,13 @@ import Dashboard from "../views/Control/Dashboard.svelte";
     export let location = "";
     export let control;
 
-    const bot = { 
-        api: {
-            address: localStorage.getItem("api-address") || "localhost",
-            port: localStorage.getItem("api-port") || 8080
-        },
-        drinks: {},
-        ingredients: {},
-        config: {}
-    }
-
-    onMount(async () => {
-        try {
-            const drinkRequest = await fetch(`http://${bot.api.address}:${bot.api.port}/drinks`);
-            bot.drinks = await drinkRequest.json();
-
-            const ingredientRequest = await fetch(`http://${bot.api.address}:${bot.api.port}/ingredients`);
-            bot.ingredients = await ingredientRequest.json();
-
-            const configRequest = await fetch(`http://${bot.api.address}:${bot.api.port}/config`);
-            bot.config = await configRequest.json();
-        } catch (err) {
-            console.log(err);
-        }
-    });
 </script>
 
 <div>
     <Sidebar {location} />
     <div class="relative md:ml-64 bg-slate-100 dark:bg-slate-800">
         <Navbar {location} />
-        <HeaderStats {bot} />
+        <HeaderStats />
         <div class="px-4 md:px-10 mx-auto w-full -m-24">
             <Router url={control}>
                 <Route path="/dashboard" component={Dashboard} />    
--- src/stores/bot.js
+++ src/stores/bot.js
@@ -13,7 +13,61 @@ export const status = readable({}, function start(set) {
         }).catch(() => {
             set({});
         })
-    });
+    }, 1000);
+
+    return function stop() {
+        clearInterval(interval);
+    }
+});
+
+export const drinks = readable({}, function start(set) {
+    const interval = setInterval(async () => {
+        fetch(apiAddress + "/drinks").then((response) => {
+            response.json().then((drinks) => {
+                set(drinks);
+            }).catch(() => {
+                set({});
+            });
+        }).catch(() => {
+            set({});
+        })
+    }, 30000);
+
+    return function stop() {
+        clearInterval(interval);
+    }
+});
+
+export const ingredients = readable({}, function start(set) {
+    const interval = setInterval(async () => {
+        fetch(apiAddress + "/ingredients").then((response) => {
+            response.json().then((ingredients) => {
+                set(ingredients);
+            }).catch(() => {
+                set({});
+            });
+        }).catch(() => {
+            set({});
+        })
+    }, 30000);
+
+    return function stop() {
+        clearInterval(interval);
+    }
+});
+
+export const config = readable({}, function start(set) {
+    const interval = setInterval(async () => {
+        fetch(apiAddress + "/config").then((response) => {
+            response.json().then((config) => {
+                set(config);
+            }).catch(() => {
+                set({});
+            });
+        }).catch(() => {
+            set({});
+        })
+    }, 30000);
 
     return function stop() {
         clearInterval(interval);
