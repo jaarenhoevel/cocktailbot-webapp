326712ee9ee610db656cbaa679efcc76ae7f4778
János Arenhövel <jaarenhoevel@gmail.com>
2022-02-18 21:51:08 +0100

    Add drink status card with make button

--- src/views/Control/Drink.svelte
+++ src/views/Control/Drink.svelte
@@ -1,7 +1,15 @@
 <script>
-    import DrinkCard from "components/Control/DrinkCard.svelte";
+    import DrinkSettingsCard from "components/Control/DrinkSettingsCard.svelte";
+    import DrinkStatusCard from "components/Control/DrinkStatusCard.svelte";
 
     export let drinkId;
 </script>
 
-<DrinkCard {drinkId} />
+<div class="flex flex-wrap">
+    <div class="w-full xl:w-8/12 px-4">
+        <DrinkSettingsCard {drinkId} />
+    </div>
+    <div class="w-full xl:w-4/12 px-4">
+        <DrinkStatusCard {drinkId} />
+    </div>
+  </div>
